fbe7513f3cdbb9fdb75d789ecde256b46e1de65c
Alexander Akopov <akopov.alexander@gmail.com>
2018-03-23 14:44:43 +0300

    Swarm start

--- start_image.sh
+++ start_image.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-docker run --name="athena" \
-    --volume=/var/run/docker.sock:/var/run/docker.sock \
+#docker run --name="athena" \
+#    --volume=/var/run/docker.sock:/var/run/docker.sock \
+#    -p 3000:3000 \
+#	adn-athena:0.0.0
+
+docker service create \
+    --name athena \
+    --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
     -p 3000:3000 \
-	adn-athena:0.0.0
+	adn-athena:0.0.1
